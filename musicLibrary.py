ec3b0cb13ab291003f7d2748c74a9e5978168401
AFK-Assassin <zunaidmullick.engineer@gmail.com>
2025-04-16 14:51:40 +0530

    Adding Jarvis

---
+++ musicLibrary.py
@@ -0,0 +1,10 @@
+music = {
+
+    "skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "alone" : "https://www.youtube.com/watch?v=1-xGerv5FOk&pp=ygUFYWxvbmU%3D" ,
+    "anycode" : "https://www.youtube.com/watch?v=D0tvwS21sis&pp=ygUIYW5lY2RvdGU%3D",
+    "hall of fame" : "https://youtu.be/mk48xRzuNvA?si=t0QPQFbQt9HObEip "
+  
+
+
+}
